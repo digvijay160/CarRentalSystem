04870168f9037d3dfdaf69a6bab33e63401e78f2
digvijay160 <digvijaychauhan.160@gmail.com>
2022-11-27 19:55:16 +0530

    rentCar function added, completed Hatchback class

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 //global declarations
-int totalCars=0,rentDays;
+int carNo=0,rentDays;                   //carNo stores type of car of the available 9 cars
 float totalRent=0;
 string firstName,lastName;
 long int contactNo,licenseNo;
@@ -20,7 +20,7 @@ class Car
 
         void customerDetails(){
             if(totalRent==0){
-                cout<<"\tPlease enter customer details\n\n";
+                cout<<"\n\tPlease enter customer details\n\n";
                 cout<<"\tFirst Name: ";
                 cin>>firstName;
                 cout<<"\n\tLast Name: ";
@@ -34,15 +34,16 @@ class Car
         }
 };
 
-int Car::nSwift=2;
-int Car::nBaleno=4;
-int Car::nI20=2;
-int Car::nCity=1;
-int Car::nVerna=3;
-int Car::nCiaz=3;
-int Car::nCreta=4;
-int Car::nCompass=2;
-int Car::nThar=3;
+//intializing the number of each cars 
+int Car::nSwift=2;          //carNo:1
+int Car::nBaleno=4;         //carNo:2
+int Car::nI20=2;            //carNo:3
+int Car::nCity=1;           //carNo:4
+int Car::nVerna=3;          //carNo:5
+int Car::nCiaz=3;           //carNo:6
+int Car::nCreta=4;          //carNO:7
+int Car::nCompass=2;        //carNo:8
+int Car::nThar=3;           //carNo:9
 
 class HatchBack:public Car
 {
@@ -64,9 +65,9 @@ class HatchBack:public Car
             cout<<<"\t\tBLUE";
 
             cout<<"\nMILEAGE";
-            cout<<<"\t\t19";
-            cout<<<"\t\t16";
-            cout<<<"\t\t11";
+            cout<<<"\t\t19kmpl";
+            cout<<<"\t\t16kmpl";
+            cout<<<"\t\t11kmpl";
 
             cout<<"\nCOST";
             cout<<<"\t\t1800/day";
@@ -75,6 +76,7 @@ class HatchBack:public Car
         }
 
         void carAvailable(){        //displays name of each hatchback-car available
+            
             cout<<"\t\tHATCHBACK CARS AVAILABLE\n\n";
 
             for(int i=1;i<=nSwift;i++)
@@ -86,5 +88,62 @@ class HatchBack:public Car
             for(int i=1;i<=nI20;i++)
                 cout<<"\ti20\n";
         }
+
+        void rentCar(){             //if user chooses hatchback
+
+            cout<<"\tDO you want to rent a Hatchback Car? Enter 'Y' for yes or 'N' for no: ";
+            cin>>reply;
+
+            if(reply=='N'||reply=='y'){
+                return;
+            }
+            else{
+
+                customerDetails();
+                cout<<"\tEnter 1/2/3 to rent:\n\t1. SWIFT\n\t2.BALENO\n\t3. i20\n\t";
+                cin>>carType;
+                cout<<"\tEnter number of days you want to rent car for: ";
+                cin>>rentDays;
+                cout<<"\n";
+
+                switch(carType){
+                    case 1: if(nSwift==0){
+                                cout<<"\tSorry, Swift is not available at the moment. Choose another car.\n";
+                            }
+                            else{
+                                carNo=1;
+                                cout<<"\tTotal Rent= "<<1800*rentDays<<endl;
+                                totalRent+=1000*rentDays;
+                                nSwift--;
+                            }
+                            break;
+                    case 2: if(nBaleno==0){
+                                cout<<"\tSorry, Baleno is not available at the moment. Choose another car.\n";
+                            }
+                            else{
+                                carNo=2;
+                                cout<<"\tTotal Rent= "<<1600*rentDays<<endl;
+                                totalRent+=1000*rentDays;
+                                nBaleno--;
+                            }
+                            break;
+                    case 3: if(nI20==0){
+                                cout<<"\tSorry, i20 is not available at the moment. Choose another car.\n";
+                            }
+                            else{
+                                carNo=3;
+                                cout<<"\tTotal Rent= "<<1400*rentDays<<endl;
+                                totalRent+=1000*rentDays;
+                                nI20--;
+                            }
+                            break;
+                    default: cout<<"\tPlease Enter a valid input\n";
+                }
+            }
+        }
+};
+
+class SUV:public Car
+{
 
 };
